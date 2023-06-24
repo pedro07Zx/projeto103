202c2f8e2f888412086b3ac37934c8e61ae3f7f0
pedro07Zx <105451246+pedro07Zx@users.noreply.github.com>
2023-06-24 14:34:52 -0300

    Add files via upload

---
+++ projeto103.py
@@ -0,0 +1,37 @@
+import sys
+import time
+import random
+import os
+import shutil
+import logging
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler 
+
+from_dir = "/home/pdhqferreira/Python"
+
+class FileEventHandler(FileSystemEventHandler):
+    def on_created(self, event):
+        print(f"{event.src_path}foi criado")
+    def on_deleted(self, event):
+        print(f"{event.src_path}foi excluído")
+    def on_modified(self, event):
+        print(f"{event.src_path}foi modificado")
+    def on_moved(self, event):
+        print(f"arquivo{event.src_path}foi movido para{event.dest_path}")
+
+event_handler = FileEventHandler()
+
+observer = Observer()
+observer.schedule(event_handler,from_dir,recursive = True)
+observer.start()
+try:
+    while True:
+        time.sleep(2)
+        print("funcionando")
+except KeyboardInterrupt:
+    print("interrompido")
+    observer.stop() 
+
+
+
+
